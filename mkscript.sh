ca4a6acf6e2d8f726dab9ed3cf81ab00230aeab9
AntobioRedro <37196914+AntobioRedro@users.noreply.github.com>
2018-08-26 14:15:56 -0500

    Add files via upload

---
+++ mkscript.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#title           :mkscript.sh
+#description     :Creates an executable script.
+#author          :pedro
+#date            :20111101
+#version         :0.1    
+#==============================================================================
+
+if [ -n "$1" ] && [ -n "$2" ] && [ -n "$3" ]; then
+    title=$1
+    description=$2
+    version=$3
+    autor=$(whoami)
+    d=$(date)
+
+    printf " #!/bin/bash \n #============================================================================== \n #title           :$title  \n #description     :$description \n #author          :$autor \n #date            :$d \n #version         :$version \n #=============================================================================="> "$title"
+    
+    chmod 744 "$title"
+else
+    echo "Usage: mkscript.sh name description version"
+fi
+
