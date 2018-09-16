5db3e07b3fe658221574efe5771f824328c9864d
AntobioRedro <37196914+AntobioRedro@users.noreply.github.com>
2018-09-16 13:18:26 -0500

    Add files via upload

---
+++ users_v2/auser.sh
@@ -0,0 +1,44 @@
+ #!/bin/bash 
+ #============================================================================== 
+ #title           :auser  
+ #description     :This script can add users and quotas
+ #author          :pedro 
+ #date            :dom sep 16 12:17:26 CDT 2018 
+ #version         :2.0
+ #==============================================================================
+
+echo -n "Enter a new user login and press [ENTER]: "
+read login
+
+echo -n "Enter a user name for "$login" and press [ENTER]: "
+read username
+
+echo -n "Enter a group for "$login" and press [ENTER]: "
+read group
+
+echo -n "Enter a home directory for "$login" and press [ENTER]: "
+read home
+
+groupadd $group
+
+useradd -d $home -m -k /etc/skel -s /bin/sh -c $username -g $group $login
+passwd $login 
+
+echo -n "Add quota for "$login"? (y/n)"
+read response
+case "$response" in
+    [yY][eE][sS]|[yY]) 
+        echo -n "Enter the soft quota for "$login" and press [ENTER]:"
+        read softquota
+        
+        echo -n "Enter the hard quota for "$login" and press [ENTER]: "
+        read hardquota
+        
+        setquota -u $login $softquota $hardquota 0 0 /
+        
+        echo -n "User "$login" added with quotas"
+        ;;
+    *)
+        echo -n "User "$login" added"
+        ;;
+esac
